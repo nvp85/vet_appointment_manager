ac71e8dbe7679bbb6a01eaba24350d645b40384e
nvp85 <nvp@zhukov.al>
2022-03-02 22:44:30 -0800

    make some model fields nullable

--- appointments/models.py
+++ appointments/models.py
@@ -10,20 +10,20 @@ class Pet(models.Model):
     name = models.CharField(max_length=150, null=False, blank=False)
     type = models.CharField(max_length=20, null=False, blank=False)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, null=False, blank=False)
-    breed = models.CharField(max_length=50)
-    microchip_num = models.CharField(max_length=100)
-    microchip_location = models.CharField(max_length=150)
-    coat_color = models.CharField(max_length=50)
-    dob = models.DateField()
+    breed = models.CharField(max_length=50, null=True, blank=True)
+    microchip_num = models.CharField(max_length=100, null=True, blank=True)
+    microchip_location = models.CharField(max_length=150, null=True, blank=True)
+    coat_color = models.CharField(max_length=50, null=True, blank=True)
+    dob = models.DateField(null=True, blank=True)
 
 
 class Client(models.Model):
     full_name = models.CharField(max_length=150, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
+    email = models.EmailField(null=True, blank=True)
+    address = models.CharField(max_length=250, null=True, blank=True)
     join_date = models.DateTimeField(auto_now_add=True)
-    pets = models.ManyToManyField(Pet) # one client can have many pets and one pet can belong to several clients
+    pets = models.ManyToManyField(Pet, null=True) # one client can have many pets and one pet can belong to several clients
 
 
 class Specialist(models.Model):
@@ -38,5 +38,5 @@ class Appointment(models.Model):
     specialist = models.ForeignKey(Specialist, null=True, on_delete=models.SET_NULL)
     date_time = models.DateTimeField(null=False)
     created = models.DateTimeField(auto_now_add=True)
-    reason = models.TextField()
-    conclusion = models.TextField()
+    reason = models.TextField(null=True, blank=True)
+    conclusion = models.TextField(null=True, blank=True)
